a376a57ca5a59762562dac9043453481ad48fcf2
JJLooi <jjlooi.ljj@gmail.com>
2021-05-29 22:26:50 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const videoElement = document.getElementById("video");
+const button = document.getElementById("button");
+
+// prompt to select a media stream, pass to video element, then play
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    }
+  } catch (error) {
+    console.log("error", error);
+  }
+}
+
+button.addEventListener('click', async () => {
+  // disable button
+  button.disabled = true;
+  //start picture in picture
+  await videoElement.requestPictureInPicture();
+  //reset button
+  button.disabled = false;
+})
+
+selectMediaStream();
